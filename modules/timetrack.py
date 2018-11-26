c3c527b4272baf9e08f7d321a52f9cf29afd75b7
Frank Willeke <frank.willeke@laubwerk.com>
2018-11-26 14:44:47 +0100

    Why was timetrack.py in this branch?

--- modules/timetrack.py
+++ modules/timetrack.py
@@ -215,11 +215,11 @@ timeTrackOptions['delete'] = delete_trackingpoint
 # get_info()
 #    Return the module's info string
 #
-# check_args(log, options)
+# check_options(log, options)
 #    Return True if main function can be run, depending on the command line arguments. If not dependent on any arguments, just return True
 #    logger object and command line options dictionary are passed
 #
-# check_additional_args(log, options)
+# check_additional_options(log, options)
 #    Return True if all arguments are not only set, but also make sense
 #    logger object and command line options dictionary are passed
 #
@@ -234,13 +234,13 @@ def setup_args(optGroup):
 
 
 # Return True if args/options tell us to run this module
-def check_args(log, options, args):
+def check_options(log, options, args):
     # print options
     return options.timetrack is not None
 
 
 # Checks additional arguments and prints error messages
-def check_additional_args(log, options, args):
+def check_additional_options(log, options, args):
     option = options.timetrack
 
     if option not in timeTrackOptions:
